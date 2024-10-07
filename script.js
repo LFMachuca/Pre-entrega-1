de99ef27cd44859df71d358f310b562107777bea
LFMachuca <fmigra87@gmail.com>
2024-10-06 21:43:23 -0300

    cambios a la preentrega

--- script.js
+++ script.js
@@ -1,47 +1,74 @@
 //Declaracion de variables a utilizar
-let usuario;
-let fechaReservaSeleccionada;
-let disponibilidad = false;
+const fechasDisponibles = ['20/10/2024', '15/11/2024','24/12/2024']
+let usuarioReserva;
+let reservaSeleccionada;
 let confirmacionReserva;
 
 //Validacion de usuario
-do {
-    usuario = prompt("Ingrese su Usuario:");
-
-    if(usuario === ""){
-        alert("Ingrese un usuario");
-    }
-} while (usuario === "");
-
-
-fechaReservaSeleccionada=parseInt(prompt("Seleccione una de las siguientes fechas: \nOpcion 1: 20/10/2024\nOpcion 2: 15/11/2024\nOpcion 3: 24/12/2024"));
-//Verificacion de la fecha seleccionada
-while(disponibilidad !== true){
-
-    switch (fechaReservaSeleccionada) {
-        case 1:
-            alert("Esta fecha no esta disponible");
-            break;
-        case 2:
-            alert("Esta fecha no esta disponible");
-            break;
-         case 3:
-            disponibilidad = true;
-            break;   
-        default:
-            alert("Por ahora no tenemos disponibilidad")
-            break;
-    };
-    if(disponibilidad === true){
-        break;
-    }
-    fechaReservaSeleccionada=parseInt(prompt("Seleccione una de las siguientes fechas: \nOpcion 1: 20/10/2024\nOpcion 2: 15/11/2024\nOpcion 3: 24/12/2024"));
+function solicitarUsuario(){
+    let usuario
+    do {
+        usuario = prompt("Ingrese su Usuario:");
+    
+        if(usuario === ""){
+            alert("Ingrese un usuario");
+        }
+    } while (usuario === "");
+    return usuario;
 };
+// Mostrar fechas
+function mostrarFechas(){
+    let mensaje = "Seleccione una de las siguientes fechas:\n";
 
-//confiramcion de la seleccion.
-confirmacionReserva=confirm(usuario + " Confirma la reserva?");
-if (confirmacionReserva) {
-    alert("Su reserva ya fue confirmada.\nMuchas gracias!");
-} else {
-    alert("Una lastima. \nVuelva Prontos");
+    for(let i = 0 ; i < fechasDisponibles.length; i++){
+        mensaje+= '\nOpcion '+i+':' + fechasDisponibles[i] ;
+    }
+    return mensaje;
+};
+// seleccion de reserva
+function seleccionarReserva(){
+
+    let fechaReservaSeleccionada;
+    let disponibilidad = false;
+    
+    fechaReservaSeleccionada = parseInt(prompt(mostrarFechas()));
+    //Verificacion de la fecha seleccionada
+    while(disponibilidad !== true){
+
+        switch (fechaReservaSeleccionada) {
+            case 0:
+                alert("Esta fecha no esta disponible");
+                break;
+            case 1:
+                alert("Esta fecha no esta disponible");
+                break;
+            case 2:
+                disponibilidad = true;
+                return fechaReservaSeleccionada;
+                break;   
+            default:
+                alert("Por ahora no tenemos disponibilidad")
+                break;
+        };
+        fechaReservaSeleccionada = parseInt(prompt(mostrarFechas()));
+    }
 }
+
+function confirmarReserva(usuario){
+    return confirm (usuario+', ¿Confirma la reserva?');
+}
+
+function realizarReserva(){
+    usuarioReserva=solicitarUsuario();
+    reservaSeleccionada=seleccionarReserva();
+    confirmacionReserva=confirmarReserva(usuarioReserva);
+
+    if (confirmacionReserva) {
+        alert("Su reserva ya fue confirmada.\nMuchas gracias!");
+    } else {
+        alert("Una lastima. \nVuelva Prontos");
+    }
+}
+
+//MAIN
+realizarReserva();
